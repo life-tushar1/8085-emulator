2e42bd06595eda22eff9d7a898bc333645d02c31
life-tushar1 <tusharsharma@Tushars-MacBook-Pro.local>
2017-09-10 22:10:29 +0530

    creating a function to convert hexadecimal to int and in the load_memory first line indicates number of memory allocation

--- start.cpp
+++ start.cpp
@@ -1,12 +1,56 @@
 #include<stdio.h>
 #include<iostream>
+#include<cstdlib>
+#include<map>
+#include<math.h>
 using namespace std;
+ map <int, int> memory;
+ int a,b,c,d,e,h,l;
+ bool zero,carry,sign,overflow;
+
+int charToint(char c)
+{
+  int v=(int)(c-'0');
+  return v;
+}
+int hexAlphaInt(char c)
+{
+  int v=(int)(c-'A');
+  v=v+10;
+  return v;
+}
+
+int hexToint(string hex)
+ {int s=0;
+   for(int i=hex.length()-1;i>=0;i--)
+   {
+     if(hex[i]>='0'&&hex[i]<='9')
+     {//cout<<hex[i]<<" "<<charToint(hex[i])<<endl;
+     s=s+(charToint(hex[i])*pow(16,hex.length()-1-i));}
+     else
+     s=s+(hexAlphaInt(hex[i])*pow(16,hex.length()-1-i));
+
+   }
+   return s;
+
+ }
+
+
 int main()
 {
   FILE *fp;
   fp=freopen("load_mem.txt","r",stdin);
-  int a,b;
-  cin>>a>>b;
-  cout<<a<<endl<<b;
+  int n;
+  cin>>n;
+  while(n>0)
+  {
+    string hex="";
+    int val;
+    cin>>hex>>val;
+    int dec=hexToint(hex);
+    cout<<dec<<endl;
+    n--;
+  }
+  //cout<<a<<endl<<b;
   return 0;
 }
