bbfd4541d4b124aa750f6077df97ff43880b1eb1
life-tushar1 <tusharsharma@Tushars-MacBook-Pro.local>
2017-09-10 21:37:52 +0530

    a simple test to check if file is accessed or not

--- start.cpp
+++ start.cpp
@@ -1,6 +1,12 @@
-#include<stdio>
+#include<stdio.h>
+#include<iostream>
 using namespace std;
 int main()
 {
+  FILE *fp;
+  fp=freopen("load_mem.txt","r",stdin);
+  int a,b;
+  cin>>a>>b;
+  cout<<a<<endl<<b;
   return 0;
 }
