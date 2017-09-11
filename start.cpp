955402ec668c1c7dec8abfaed35fae7a3baed9bb
life-tushar1 <tusharsharma@Tushars-MacBook-Pro.local>
2017-09-11 21:40:20 +0530

    creation of utility header file, converting it to modular

--- start.cpp
+++ start.cpp
@@ -1,55 +1,9 @@
-#include<stdio.h>
-#include<iostream>
-#include<cstdlib>
-#include<map>
-#include<math.h>
+#include "utility.h"
 using namespace std;
- map <int, int> memory;
+map <int, int> memory;
  int a=0,b=0,c=0,d=0,e=0,h=0,l=0;
  bool flag[4]={false};
 
-int charToint(char c)
-{
-  int v=(int)(c-'0');
-  return v;
-}
-int hexAlphaInt(char c)
-{
-  int v=(int)(c-'A');
-  v=v+10;
-  return v;
-}
-
-int hexToint(string hex)
- {int s=0;
-   for(int i=hex.length()-2;i>=0;i--)
-   {
-     if(hex[i]>='0'&&hex[i]<='9')
-     {//cout<<hex[i]<<" "<<charToint(hex[i])<<endl;
-     s=s+(charToint(hex[i])*pow(16,hex.length()-2-i));}
-     else
-     s=s+(hexAlphaInt(hex[i])*pow(16,hex.length()-2-i));
-
-   }
-   return s;
-}
-int stringToint(string str)
-{
-  int s=0;
-  for(int i=0;i<str.length();i++)
-  s=(s*10)+charToint(str[i]);
-  return s;
-}
-
-void display()
-{
-  map <int, int> :: iterator it;
-    for (it= memory.begin(); it != memory.end(); ++it)
-      {
-          cout << it->first <<  '\t' << it->second << '\n';
-      }
-  cout<<"A "<<a<<endl<<"B "<<b<<endl<<"C "<<c<<endl<<"D "<<d<<endl<<"E "<<e<<endl<<"H "<<h<<endl<<"L "<<l<<endl;
-}
 
 int main()
 {
@@ -388,6 +342,6 @@ int main()
   }
 
 }
-  display();
+  display(memory,a,b,c,d,e,h,l);
   return 0;
 }
---
+++ utility.h
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<iostream>
+#include<cstdlib>
+#include<map>
+#include<math.h>
+using namespace std;
+int charToint(char c)
+{
+  int v=(int)(c-'0');
+  return v;
+}
+int hexAlphaInt(char c)
+{
+  int v=(int)(c-'A');
+  v=v+10;
+  return v;
+}
+
+int hexToint(string hex)
+ {int s=0;
+   for(int i=hex.length()-2;i>=0;i--)
+   {
+     if(hex[i]>='0'&&hex[i]<='9')
+     {//cout<<hex[i]<<" "<<charToint(hex[i])<<endl;
+     s=s+(charToint(hex[i])*pow(16,hex.length()-2-i));}
+     else
+     s=s+(hexAlphaInt(hex[i])*pow(16,hex.length()-2-i));
+
+   }
+   return s;
+}
+int stringToint(string str)
+{
+  int s=0;
+  for(int i=0;i<str.length();i++)
+  s=(s*10)+charToint(str[i]);
+  return s;
+}
+
+void display(map <int, int> memory,int a,int b,int c,int d,int e,int h,int l)
+{
+  map <int, int> :: iterator it;
+    for (it= memory.begin(); it != memory.end(); ++it)
+      {
+          cout << it->first <<  '\t' << it->second << '\n';
+      }
+  cout<<"A "<<a<<endl<<"B "<<b<<endl<<"C "<<c<<endl<<"D "<<d<<endl<<"E "<<e<<endl<<"H "<<h<<endl<<"L "<<l<<endl;
+}
