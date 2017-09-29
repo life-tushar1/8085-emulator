0b7bba91408945cc9e37b723e6e315057c7b1303
life-tushar1 <tusharsharma@Tushars-MacBook-Pro.local>
2017-09-29 21:00:06 +0530

    code optimized

--- cmdUtility.h
+++ cmdUtility.h
@@ -3,4 +3,5 @@
 #define _cmdUtility_h
 void switchAssi(char ,int ,int *,int *,int *,int *,int *,int *,int *);
 void switchMemAccess(char ,int , std::map <int, int> &,int *,int *,int *,int *,int *,int *,int *);
+void switchRegAccess(char ,char,int *,int *,int *,int *,int *,int *,int *);
 #endif
